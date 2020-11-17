1975adcd77e598c0d10b28dca67d5646680b04c4
willow <wsmoyer313@gmail.com>
2020-11-17 14:47:24 -0600

    initial commit

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,3 @@
 module.exports = {
-    // chainWebpack: config => {
-    //     // GraphQL Loader
-    //     config.module
-    //       .rule('obj')
-    //       .test(/\.obj$/)
-    //       .use('webpack-obj-loader')
-    //         .loader('webpack-obj-loader')
-    //         .end()
-    //   }
+  publicPath: '/point-layer/'
   }
